936c0431648b3a3c78ed17eb1c16e7e5d95d3a5c
Hussain Taj <hussaintajwork@gmail.com>
2023-05-10 02:33:24 +0500

    Add BASE_SITE_URL Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,31 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import eslint from "vite-plugin-eslint";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "src/data/*.json",
-          dest: "data/",
-        },
-      ],
-    }),
-    react(),
-    eslint(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.BASE_SITE_URL,
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: "src/data/*.json",
+            dest: "data/",
+          },
+        ],
+      }),
+      react(),
+      eslint(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
+      },
     },
-  },
+  };
 });
