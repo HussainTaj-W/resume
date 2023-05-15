7a20e6f444f97f22da6a4b029724b625a44c8901
Hussain Taj <hussaintajwork@gmail.com>
2023-05-15 16:36:55 +0500

    Add metadata

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import eslint from "vite-plugin-eslint";
+import Sitemap from "vite-plugin-sitemap";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import pluginYaml from "vite-plugin-yaml2";
 
@@ -23,6 +24,7 @@ export default defineConfig(({ mode }) => {
           },
         ],
       }),
+      Sitemap(),
     ],
     resolve: {
       alias: {
